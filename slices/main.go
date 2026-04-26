b1e6c92aefbc14280c092f46ce0eea0f24bdb066
Anupam <anupam.r2301@gmail.com>
2026-04-26 21:32:02 +0530

    learned about slices

---
+++ slices/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// numbers := []int{1, 2, 3, 4, 5}
+	// numbers = append(numbers, 10,12,34,45)
+	// fmt.Println("Numbers :",numbers)
+	// fmt.Printf("Numbers has datatype of : %T\n",numbers)
+	// fmt.Println("Length",len(numbers))
+	names := []string{}
+	fmt.Println("This Name:",names)
+}
