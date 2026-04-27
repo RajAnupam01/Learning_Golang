ea8fdb16313f6c80fb238ce5e466634b393ffc1d
Anupam <anupam.r2301@gmail.com>
2026-04-27 09:48:03 +0530

    learned about if-else

---
+++ if-else/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// x := 10
+	// if x == 0 {
+	// 	fmt.Println("No worries.")
+	// }else{
+	// 	fmt.Println("Good Morning.")
+	// }
+
+	z := 10
+
+	if z > 10 {
+		fmt.Println("z is greater  than 10")
+	} else if z < 10{
+		fmt.Println("z is less than 10")
+	} else{
+		fmt.Println("Z is 10.")
+	}
+}
