1d7982e8838ab227e2038565221889ba251a4898
Anupam <anupam.r2301@gmail.com>
2026-04-29 22:57:23 +0530

    learning for-loop

--- for-loop/main.go
+++ for-loop/main.go
@@ -3,8 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	count := 10
-	for i := 0; i < count; i++ {
-		fmt.Println(i)
+	
+	numbers := []int {1,2,3,4,5}
+	for i,val := range numbers{
+		fmt.Printf("Index of Numbers is %d, and value is %d\n",i,val)
+	}
+
+	data := "Hello world"
+
+	for x, char:= range data{
+		fmt.Printf("Index of Data is %d, and char is %c\n", x, char)
 	}
 }
